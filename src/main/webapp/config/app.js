362a32c8369ced16f24efac81cfa575606da5d21
GulabThakur <gulabthakur238@gmail.com>
2018-02-23 12:26:27 +0530

    heroku resovel change

--- src/main/webapp/config/app.js
+++ src/main/webapp/config/app.js
@@ -41,9 +41,9 @@ app.config( function($stateProvider, $urlRouterProvider ,$authProvider) {
 		url : '/login',
 		// template: 'login',
 		templateUrl : 'template/login.html',
-		resolve: {
+		/*resolve: {
 		        skipIfAuthenticated: skipIfAuthenticated
-		      },
+		      },*/
 		controller : 'loginController'
 	})
 	// this code will be use for register CrtL and load HtmL..
